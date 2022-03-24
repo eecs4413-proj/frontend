64bc4c19cc1c7318e5c5727caea996f88c1aac0a
ParssaKhazra <parssa.khazra@gmail.com>
2022-03-24 19:39:22 -0400

    edited cart page and fixed a bug

--- src/components/cart/Cart.js
+++ src/components/cart/Cart.js
@@ -1,29 +1,38 @@
 import React from "react";
-import { TextField, Typography, Grid, Box } from "@mui/material";
+import { TextField, Typography, Grid, Box, Stack, Container } from "@mui/material";
 
-import CatalogItem from '../catalog/item';
-const items = require('../../items.json');
+import CatalogItem from "../catalog/item";
+const items = require("../../items.json");
 
-const cart = ()=> {
-  
-
-
+const cart = () => {
   return (
     <>
-     <Typography variant="h3" > My Cart </Typography>
-    <TextField type = "number" InputProps={{ inputProps: { min: 0, max: 10 } }}/>
-    <Grid item xs={12}>
-        <Box m={2} pt={3}>
-          <Grid container direction="row"justify="center" spacing={6}>
-              {items.map((product) => (
-                  <Grid item key={product.id} xs={12} sm={6} md={4} >
-                      <CatalogItem item={product} />
-                  </Grid>
-              ))}
-          </Grid>      
-        </Box>
-      </Grid>
+    <Container  sx={{ width: '50%' }}>
+      
+      <Typography style ={{textAlign: "center"}} variant="h3"> My Cart </Typography>
+      <br/>
+
+      <Stack container direction="column" justify="center" spacing={6}>
+        {items.map((product) => (
+          <Stack item key={product.id} xs={12} sm={6} md={4} direction="column">
+            <CatalogItem item={product} />
+            <TextField
+              justify="right"
+              id={product.id}
+              type="number"
+              defaultValue ={1}
+              InputProps={{ inputProps: { min: 0, max: 10 } }}
+            />
+          </Stack>
+        ))}
+      </Stack>
+      <br />
+      <div style={{ display: "flex" }}>
+        <button style={{ marginLeft: "auto" }}>checkout</button>
+      </div>
+      <br/>
+      </Container>
     </>
   );
-}
+};
 export default cart;
--- src/components/login/LoginContainer.js
+++ src/components/login/LoginContainer.js
@@ -2,7 +2,7 @@ import React from 'react'
 import Login from './Login'
 import style from "./LoginStyle.module.css";
 import pic from "./loginImage.jpg";
-import { Stack } from 'react-bootstrap';
+
 
 function LoginContainer() {
     return (
